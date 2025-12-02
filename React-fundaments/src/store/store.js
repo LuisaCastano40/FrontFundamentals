d95f481e16761464e28b814b15ce43545e926e8d
LuisaCastano40 <loise.0607@gmail.com>
2025-12-01 23:57:48 -0500

    create: components, files, en redux store con RTK

---
+++ React-fundaments/src/store/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+/**
+ * STORE DE REDUX
+ * 
+ * El store es el contenedor global del estado.
+ * configureStore simplifica la configuración:
+ * - Incluye Redux DevTools automáticamente
+ * - Configura middleware por defecto
+ * - Combina reducers
+ */
+
+export const store = configureStore({
+
+});
